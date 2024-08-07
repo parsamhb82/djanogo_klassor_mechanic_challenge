2fbad09ed39bfb6036bfb27c8c37c11fdb2faf3c
aria_afsar <aria.afsar@gmail.com>
2024-08-07 11:38:00 +0330

    created a liist in grage manager class

--- garage.py
+++ garage.py
@@ -1,6 +1,7 @@
 class grage :
-    def __init__(self) -> None:
-        pass
+    def __init__(self , x , y) :
+        self.x = x
+        self.y = y
 class grage_manager :
-    def __init__(self) -> None:
-        pass
+    def __init__(self) :
+        self.grage_list = []
