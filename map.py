e253e88f6c42b1af4e0f1537201f866614d71db2
Mohammad Mahan Bidi <mahanbidi771@gmail.com>
2024-08-07 11:41:24 +0330

    change 2

--- map.py
+++ map.py
@@ -1,8 +1,23 @@
-class Node : 
-    pass
+class Node :
+    def __init__(self , x , y) -> None:
+        self.x = x
+        self.y = y 
+
+
+class Maps : 
+    def __init__(self) -> None:
+        self.nodes_list = []
+
+    def return_jason_to_map (self) : 
+        pass 
 
 class Garage : 
-    pass
+    
+    def __init__(self , x , y) -> None:
+        self.x = x
+        self.y = y 
 
 class Garage_manager : 
-    pass 
+ 
+    def __init__(self) -> None:
+        self.garage_list = []
