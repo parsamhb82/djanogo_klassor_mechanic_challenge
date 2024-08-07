0c4cc49ff5b049b209a0f856f1264d7e938a7d6e
Mohammad Mahan Bidi <mahanbidi771@gmail.com>
2024-08-07 11:43:18 +0330

    edit

--- garage.py
+++ garage.py
@@ -1,7 +1,11 @@
-class grage :
+class Grage :
+
     def __init__(self , x , y) :
         self.x = x
         self.y = y
-class grage_manager :
+
+
+class Grage_manager :
+
     def __init__(self) :
         self.grage_list = []
